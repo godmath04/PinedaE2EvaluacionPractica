3f2c3d13f102af41178668fccc197dedea2ada14
Laboratorio <laboratorio@udla-ec.int>
2024-06-20 08:56:50 -0500

    Casi esta, solo falta unas dos cosaS

--- Main.java
+++ Main.java
@@ -2,12 +2,46 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-
-        float tarifa = Float.parseFloat(JOptionPane.showInputDialog("Ingrese la tarifa inicial"));
+        int option = 0;
+        double tarifa = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la tarifa inicial"));
         int horaInicio = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la hora inicial"));
         Parqueadero parkob = new Parqueadero(tarifa, horaInicio);
 
-        parkob.info();
+        if (horaInicio >= 8 && horaInicio <= 18) {
+            while (option != 5) {
+                option = Integer.parseInt(JOptionPane.showInputDialog("Ingrese su opcion: \n1. Ingresar carro" +
+                        "\n2. Sacar un carro" + "\n3. Mostrar puestos e ingresos" +
+                        "\n4. Adelantar hora" +
+                        "\n5. Salir"));
+                switch (option) {
+                    case 1:
+                        //Ahora una vez que esta funcionando el parqueadero vamos a ingresar carros
+                        String placa = JOptionPane.showInputDialog(null, "Ingrese la placa del carro:");
+                        int horaEntrada = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese la hora de entrada:"));
+                        if (horaEntrada < 8 || horaEntrada > 18) {
+                            JOptionPane.showMessageDialog(null, "Hora de entrada no válida. Debe ser entre 8:00 y 18:00.");
+                            return;
+                        }
+                        parkob.ingresoCarro(placa, horaEntrada);
+                        break;
+
+                    case 2:
+                        String placa2 = JOptionPane.showInputDialog(null, "Ingrese la placa del carro a sacar:");
+                        parkob.darSalidaCarro(placa2);
+                        break;
+                    case 3:
+                        JOptionPane.showMessageDialog(null, "Puestos disponibles: " + parkob.consultarPuestosDisponibles() +
+                                "\nIngresos totales: $" + parkob.getIngresos());
+                        break;
+                    case 4:
+                        int horas = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese las horas a avanzar:"));
+                        parkob.avanzarReloj(horas);
+                        break;
+
+                }
+            }
+
+        }
     }
 
 }
--- Parqueadero.java
+++ Parqueadero.java
@@ -1,60 +1,125 @@
 import javax.swing.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Parqueadero {
     //Creacion de una lista que almacena el objeto de Puesto
     private List<Puesto> puestos;
-    private float tarifaInicial;
-    private int horaInicial;
-    private float ingresos;
+    private double tarifa;
+    private int hora;
+    private double ingresos;
 
-    // Constructor
-    public Parqueadero(float tarifaInicial, int horaInicial){
-        this.puestos = null;
-        this.tarifaInicial = tarifaInicial;
+    //Constructor para que se asginen los valores
+    public Parqueadero(double tarifaInicial, int horaInicial){
+        this.tarifa = tarifaInicial;
         if (horaInicial >= 8 && horaInicial <= 18){
-            this.horaInicial = horaInicial;
-            JOptionPane.showMessageDialog(null, "El parqueadero se abre a las: " + horaInicial + " con una tarifa de: $ " + tarifaInicial);
+            this.hora = horaInicial;
         }else {
-            JOptionPane.showMessageDialog(null, "Se encuentra fuera de la hora de funcionamiento.");
-        }
-        this.ingresos = 0;
-        //Creacion de la lista
-        for (int i = 0; i <5 ; i++){
-            puestos.add(new Puesto(i));
-        }
 
+            JOptionPane.showMessageDialog(null, "La hora de inicio es incorrecta.");
+        }
+        this.puestos = new ArrayList<>();
+        for (int i = 1; i<=3; i++){
+            this.puestos.add(new Puesto(i));
+        }
+        this.ingresos = 0.0;
     }
 
+
+    // Metodo de ingreso de vehiculo
+    public void ingresoCarro(String placa, int horaEntrada){
+            if (horaEntrada < 8 || horaEntrada > 18) {
+                JOptionPane.showMessageDialog(null, "Se encuentra fuera de horario.");
+                return;
+            }
+
+            //Verifica disponibidlidad y placa igual
+            for (int i = 0; i < puestos.size(); i++) {
+                Puesto puesto = puestos.get(i);
+
+                //Si el puesto no esta disponible
+                if (!puesto.estaDisponible() && puesto.getCarro().getPlaca().equals(placa)) {
+                    JOptionPane.showMessageDialog(null, "Ya hay un carro con esa placa");
+                    return;
+                }
+
+            }
+
+            for (int i = 0; i < puestos.size(); i++) {
+                Puesto puesto = puestos.get(i);
+                if (puesto.estaDisponible()) {
+                    puesto.asignarCarro(new Carro(placa, horaEntrada));
+                    JOptionPane.showMessageDialog(null, "Carro ingresado en el puesto: " + puesto.getNumero());
+                    return;
+                }
+            }
+
+            JOptionPane.showMessageDialog(null, "No hay puestos");
+    }
+
+    ///Metodo para que salga el carro
+    public void darSalidaCarro(String placa) {
+        for (int i = 0; i < puestos.size(); i++) {
+            Puesto puesto = puestos.get(i);
+            if (!puesto.estaDisponible() && puesto.getCarro().getPlaca().equals(placa)) {
+                int horaActual = hora;
+                int horaEntrada = puesto.getCarro().getHoraEntrada();
+
+                //Calculo en horas
+                int horasEstacionado;
+                if (horaActual >= horaEntrada) {
+                    horasEstacionado = horaActual - horaEntrada;
+                } else {
+                    horasEstacionado = (24 - horaEntrada) + horaActual;
+                }
+                double costo = horasEstacionado * tarifa;
+                JOptionPane.showMessageDialog(null, "El costo por estacionamiento es: $" + costo);
+                ingresos += costo; // Actualiza los ingresos
+                puesto.liberarCarro();
+                return;
+            }
+        }
+    }
 
 
     //Metodo para mostrar informacion
-    public String info(){
+    public String info() {
         StringBuilder sb = new StringBuilder();
-        sb.append("La tarifa inicial es: " + tarifaInicial);
-        sb.append("\nLa hora inicial es: " + horaInicial);
-        return   sb.toString();
+        sb.append("La tarifa inicial es: " + tarifa);
+        sb.append("\nLa hora inicial es: " + hora);
+        sb.append("\nCarros en el parqueadero:" + puestos.size());
+    return toString();
     }
 
-    //Metodo para ingresar carro la parqueadero
-    public void ingresoCarro(String placa, int horaEntrada) {
-        if (horaEntrada < 8 || horaEntrada > 18) {
-            JOptionPane.showMessageDialog(null, "Hora invalida. Vuelva en horario disponible.");
-            return;
-        }
-
-    //Verifica disponibidlidad y placa igual
+    //Metodo para mostrar puestos disponibles
+    public int consultarPuestosDisponibles(){
+        int disponibles = 0;
         for (int i = 0; i < puestos.size(); i++){
-        // Nos referimos a la clase Puesto y scamos el nombre de la variable puesto para referirse a un objeto de esa clase
-        Puesto puesto = puestos.get(i);
-
-        //Si el puesto no esta disponible
-        if( !puesto.estaDisponible() && puesto.getCarro().getPlaca().equals(placa)){
-            JOptionPane.showMessageDialog(null, "Ya hay un carro con esa placa");
-            return;
+            Puesto puesto = puestos.get(i);
+            if (puesto.estaDisponible()){
+                disponibles++;
+            }
         }
+        return disponibles;
+    }
 
+    public double getIngresos(){
+        return ingresos;
+    }
+
+
+    //Metodo para avanzar el relok
+    // Avanzar el reloj
+    public void avanzarReloj(int horas) {
+        if (hora + horas > 18) {
+            JOptionPane.showMessageDialog(null, "No se puede avanzar el reloj más allá de las 21:00.");
+        } else {
+            hora += horas;
+            JOptionPane.showMessageDialog(null,"La hora actual es: " + hora + ":00.");
         }
     }
 
+    public int getHora() {
+        return hora;
+    }
 }
